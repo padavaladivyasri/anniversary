b21bdbe0a68810d67b97e42565bf0a6a1d156345
Padavala Divya Sri <padavala.divyasri@gmail.com>
2026-08-09 19:04:38 +0000

    Fix base path for root deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/anniversary/',
+  base: '/',  // Change from '/anniversary/' to '/'
   plugins: [react()],
 })
